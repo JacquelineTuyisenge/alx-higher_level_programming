4fbdf2719e1c3f37ca7292a66e60b8603c458657
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-03-07 12:28:22 -0800

    if/else loop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for digit_one in range(0, 9):
-    for digit_two in range(digit_one + 1, 10):
-        if digit_one == 8:
-            print("{}{}".format(digit_one, digit_two))
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        if num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
         else:
-            print("{}{}".format(digit_one, digit_two), end=", ")
+            print("{}{}".format(num1, num2), end=", ")
